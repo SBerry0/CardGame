e8eb3189721e03f6c51f9df71c8f78583fe0a6a3
Sohum Berry <sohumberry@gmail.com>
2023-11-27 13:51:51 -0800

    Created getters and setters for card

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Card {
+public class Card {
+    private int point;
+    private String suit;
+    private String rank;
+
+    public Card(String suit, String rank, int point) {
+        this.point = point;
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public void setSuit(String newSuit) {
+        suit = newSuit;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setRank(String newRank) {
+        rank = newRank;
+    }
+    public String getRank() {
+        return rank;
+    }
+
+    public void setPoint(int newPoint) {
+        point = newPoint;
+    }
+    public int getPoint() {
+        return point;
+    }
+
+    public String toString() {
+        return rank + " of " + suit;
+    }
 }
